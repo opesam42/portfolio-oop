65b34093ccd4ccec0bc3f0de62265677de264174
Gbenga Opeyemi <opesam42@gmail.com>
2025-02-05 05:57:46 +0100

    change to a new db

--- apps/core/Database.php
+++ apps/core/Database.php
@@ -4,8 +4,23 @@ class Database{
     private function connect(){
         try{
             $str = "mysql:host=" . DB_HOST . "; dbname=" . DB_NAME;
-            $conn = new PDO($str, DB_USER, DB_PASS);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // $sslCertPath = __DIR__ . "/../certs/ca.crt";
+            $sslCertPath = "../../app/certs/ca.pem";
+            if (!$sslCertPath) {
+                 error_log("❌ Error: SSL CA certificate (ca.pem) not found!");
+                die("Error: SSL CA certificate (ca.pem) not found!");
+            }
+
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                PDO::MYSQL_ATTR_SSL_CA => $sslCertPath, // Path to your CA certificate
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+            ];
+            // $conn = new PDO($str, DB_USER, DB_PASS);
+            // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // $conn->setAttribute(PDO::AFTER_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            $conn = new PDO($str, DB_USER, DB_PASS, $options);
             return $conn;
         } catch(PDOException $e){
             die("Connection fail: " . $e->getMessage());
