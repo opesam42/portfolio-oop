74033655fe7b766303129272e9edb6c4867dfb87
Gbenga Opeyemi <opesam42@gmail.com>
2025-08-22 08:43:05 +0100

    fix(env): correct .env file path for dotenv loader

--- apps/core/config.php
+++ apps/core/config.php
@@ -1,8 +1,11 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../public');
-$dotenv->load();
+$dotenvPath = __DIR__ . '/../../';
+if (file_exists($dotenvPath . '.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable($dotenvPath);
+    $dotenv->load();
+}
 
 define("DB_NAME", $_ENV['DBDATABASE']);
 define("DB_HOST", $_ENV['DBHOST']);
